0aec71c3688300db15683f6790782744e6be2f21
nadezhda <nadezhda277135@gmail.com>
2016-12-24 23:39:10 +0200

    techcard

--- src/main/resources/static/scripts/uiTechcard.js
+++ src/main/resources/static/scripts/uiTechcard.js
@@ -72,6 +72,26 @@ $( document ).ready(function() {
     getAllTechcard(function (response) {
         console.log("filling table with data..." + JSON.stringify(response))
         table = $("#tableTechcard").DataTable( {
+            initComplete: function () {
+                this.api().columns().every( function () {
+                    var column = this;
+                    var select = $('<select><option value=""></option></select>')
+                        .appendTo( $(column.footer()).empty() )
+                        .on( 'change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+
+                            column
+                                .search( val ? '^'+val+'$' : '', true, false )
+                                .draw();
+                        } );
+
+                    column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' )
+                    } );
+                } );
+            },
             responsive: true,
             "processing": true,
             "bAutoWidth" : false,
